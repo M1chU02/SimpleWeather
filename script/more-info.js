10c0c18515d7593762af504ad2d90ec5c0431bb6
MichU02 <m.mikolajewski02@gmail.com>
2023-07-30 10:04:59 +0200

    version 1.1

--- script/more-info.js
+++ script/more-info.js
@@ -1,37 +1,31 @@
-function more(city, api)
-{
-displayMore(city, api);
+function more(city, api) {
+  displayMore(city, api);
 
-function displayMore() 
-{
-        const base = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${api}&units=metric`;
-        fetch(base)
-          .then((response) => {
-            return response.json();
-          })
-          .then((data) => {
-              const temperature = data.main.feels_like;
-              const humidity = data.main.humidity;
-              const windspeed = data.wind.speed;
-              const pressure = data.main.pressure;
+  function displayMore() {
+    const base = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${api}&units=metric`;
+    fetch(base)
+      .then((response) => {
+        return response.json();
+      })
+      .then((data) => {
+        const temperature = data.main.feels_like;
+        const humidity = data.main.humidity;
+        const windspeed = data.wind.speed;
+        const pressure = data.main.pressure;
 
-              const temperature_div = document.getElementById("temperature_div");
-              const humidity_div = document.getElementById("humidity_div");
-              const wind_div = document.getElementById("wind_div");
-              const pressure_div = document.getElementById("pressure_div");
+        const temperature_div = document.getElementById("temperature_div");
+        const humidity_div = document.getElementById("humidity_div");
+        const wind_div = document.getElementById("wind_div");
+        const pressure_div = document.getElementById("pressure_div");
 
-              temperature_div.innerHTML = 
-              `<p>Perceptible temperature ${temperature}°C</p>
+        temperature_div.innerHTML = `<p>Perceptible temperature ${temperature}°C</p>
               <img class="icons" src="/img/tempicon.png">`;
-              humidity_div.innerHTML = 
-              `<p>Humidity ${humidity}%</p><br>
+        humidity_div.innerHTML = `<p>Humidity ${humidity}%</p><br>
               <img class="icons" src="/img/humidityicon.png">`;
-              wind_div.innerHTML = 
-              `<p>Wind speed ${windspeed} m/s</p>
+        wind_div.innerHTML = `<p>Wind speed ${windspeed} m/s</p>
               <img class="icons" src="/img/windicon.png">`;
-              pressure_div.innerHTML = 
-              `<p>Pressure ${pressure} hPa</p>
+        pressure_div.innerHTML = `<p>Pressure ${pressure} hPa</p>
               <img class="icons" src="/img/pressureicon.png">`;
-          })
-        }
+      });
+  }
 }
--- script/search.js
+++ script/search.js
@@ -1,32 +1,48 @@
-const api = '3d0e75b1809f78f44935c36ada7b307f';
-function weatherApp()
-{
-    var cityForm = document.getElementById("cityform").value;
-    if(cityForm!="")
-    {
+const api = "3d0e75b1809f78f44935c36ada7b307f";
+function weatherApp() {
+  var cityForm = document.getElementById("cityform").value;
+  const base = `https://api.openweathermap.org/data/2.5/weather?q=${cityForm}&appid=${api}&units=metric`;
+  fetch(base)
+    .then((response) => {
+      return response.json();
+    })
+    .then((data) => {
+      const errorcheck = data.cod;
+
+      if (cityForm != "" && errorcheck != "404") {
         let input = document.getElementById("cityform");
         input.value = "";
         main(cityForm, api);
         hour(cityForm, api);
-        more(cityForm, api); 
+        more(cityForm, api);
         var mainbody = document.getElementById("all");
         var formbody = document.getElementById("weatherform");
         mainbody.style.visibility = "visible";
-        formbody.style.visibility = "hidden";   
-    }
-    else
-    {
+        formbody.style.visibility = "hidden";
+      } else {
+        let input = document.getElementById("cityform");
         location.reload;
-        console.log("blad");
-    }
+        input.value = "";
+        const error_msg = document.createElement("p");
+        error_msg.innerHTML = "Wrong location";
+        const error_div = document.getElementById("errordiv");
+        error_div.appendChild(error_msg);
+      }
+    });
 }
 
-function changeCity()
-{
-    var cityinput = prompt("Enter city: ");
-    const hourly_div = document.getElementById("hourlyforecast");
-    hourly_div.innerHTML = "";
-    main(cityinput, api);
-    hour(cityinput, api);
-    more(cityinput, api); 
+function changeCity() {
+  var cityinput = prompt("Enter city: ");
+  const hourly_div = document.getElementById("hourlyforecast");
+  hourly_div.innerHTML = "";
+  main(cityinput, api);
+  hour(cityinput, api);
+  more(cityinput, api);
 }
+
+document.querySelector("#form").addEventListener("keypress", function (e) {
+  if (e.key === "Enter") {
+    event.preventDefault();
+    weatherApp();
+  }
+});
